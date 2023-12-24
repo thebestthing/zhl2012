031394c634bf71221767a104e21160a771efa08c
thebestthing <40907562+thebestthing@users.noreply.github.com>
2023-12-24 18:50:50 +0800

    Create wf1.js

---
+++ wf1.js
@@ -0,0 +1,169 @@
+
+// 转换时间: 2023/12/24 18:48:23
+// 兼容性转换
+if (typeof $request !== 'undefined') {
+  const lowerCaseRequestHeaders = Object.fromEntries(
+    Object.entries($request.headers).map(([k, v]) => [k.toLowerCase(), v])
+  );
+
+  $request.headers = new Proxy(lowerCaseRequestHeaders, {
+    get: function (target, propKey, receiver) {
+      return Reflect.get(target, propKey.toLowerCase(), receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      return Reflect.set(target, propKey.toLowerCase(), value, receiver);
+    },
+  });
+}
+if (typeof $response !== 'undefined') {
+  const lowerCaseResponseHeaders = Object.fromEntries(
+    Object.entries($response.headers).map(([k, v]) => [k.toLowerCase(), v])
+  );
+
+  $response.headers = new Proxy(lowerCaseResponseHeaders, {
+    get: function (target, propKey, receiver) {
+      return Reflect.get(target, propKey.toLowerCase(), receiver);
+    },
+    set: function (target, propKey, value, receiver) {
+      return Reflect.set(target, propKey.toLowerCase(), value, receiver);
+    },
+  });
+}
+
+
+// QX 相关
+var setInterval = () => {}
+var clearInterval = () => {}
+var $task = {
+  fetch: url => {
+    return new Promise((resolve, reject) => {
+      if (url.method == 'POST') {
+        $httpClient.post(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      } else {
+        $httpClient.get(url, (error, response, data) => {
+          if (response) {
+            response.body = data
+            resolve(response, {
+              error: error,
+            })
+          } else {
+            resolve(null, {
+              error: error,
+            })
+          }
+        })
+      }
+    })
+  },
+}
+
+var $prefs = {
+  removeValueForKey: key => {
+    return $persistentStore.write(null, key)
+  },
+  valueForKey: key => {
+    return $persistentStore.read(key)
+  },
+  setValueForKey: (val, key) => {
+    return $persistentStore.write(val, key)
+  },
+}
+
+var $notify = (title = '', subt = '', desc = '', opts) => {
+  const toEnvOpts = (rawopts) => {
+    if (!rawopts) return rawopts 
+    if (typeof rawopts === 'string') {
+      if ('undefined' !== typeof $loon) return rawopts
+      else if('undefined' !== typeof $rocket) return rawopts
+      else return { url: rawopts }
+    } else if (typeof rawopts === 'object') {
+      if ('undefined' !== typeof $loon) {
+        let openUrl = rawopts.openUrl || rawopts.url || rawopts['open-url']
+        let mediaUrl = rawopts.mediaUrl || rawopts['media-url']
+        return { openUrl, mediaUrl }
+      } else {
+        let openUrl = rawopts.url || rawopts.openUrl || rawopts['open-url']
+        if('undefined' !== typeof $rocket) return openUrl
+        return { url: openUrl }
+      }
+    } else {
+      return undefined
+    }
+  }
+  console.log(title, subt, desc, toEnvOpts(opts))
+  $notification.post(title, subt, desc, toEnvOpts(opts))
+}
+var _scriptSonverterOriginalDone = $done
+var _scriptSonverterDone = (val = {}) => {
+  let result
+  if (
+    (typeof $request !== 'undefined' &&
+    typeof val === 'object' &&
+    typeof val.status !== 'undefined' &&
+    typeof val.headers !== 'undefined' &&
+    typeof val.body !== 'undefined') || false
+  ) {
+    try {
+      for (const part of val?.status?.split(' ')) {
+        const statusCode = parseInt(part, 10)
+        if (!isNaN(statusCode)) {
+          val.status = statusCode
+          break
+        }
+      }
+    } catch (e) {}
+    if (!val.status) {
+      val.status = 200
+    }
+    if (!val.headers) {
+      val.headers = {
+        'Content-Type': 'text/plain; charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      }
+    }
+    result = { response: val }
+  } else {
+    result = val
+  }
+  console.log('$done')
+  try {
+    console.log(JSON.stringify(result))
+  } catch (e) {
+    console.log(result)
+  }
+  _scriptSonverterOriginalDone(result)
+}
+var window = globalThis
+window.$done = _scriptSonverterDone
+var global = globalThis
+global.$done = _scriptSonverterDone
+
+var body = $response.body; 
+var obj = JSON.parse(body); 
+obj= {
+ "account": {
+    "info": {
+      "hasError": false,
+      "error": null,
+      "name": "Cracked",
+      "email": "Cracked by xxx"
+    },
+    "status": {
+      "hasError": false,
+      "error": null,
+      "signedIn": true,
+      "pro": true,
+      "proForStudents": true,
