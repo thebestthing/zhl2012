459ccfea4f2fa1965cc407f503edc559b63cfdd1
thebestthing <40907562+thebestthing@users.noreply.github.com>
2024-06-08 20:49:46 +0800

    Update 15m.js

--- 15m.js
+++ 15m.js
@@ -17,26 +17,30 @@ hostname = 18m.jinyemimi.com
 var body = $response.body;
 var obj = JSON.parse(body);
  
+// 定义一个对象，假设这是从服务器获取的
 obj = {
-    "code": 1,
-    "msg": "",
-    "time": "1717888888",
-    "data": {
-        "id": 16036,
-        "username": "13727968870",
-        "nickname": "在我梦里",
-        "mobile": "13727968870",
-        "avatar": "http://15m.xiexiebama.com/user/face/20220412/cb2476aacebd0bd86b7c4960f2b324bf.rosi",
-        "money": 9999,
-        "status": "normal",
-        "vip_sta": "0",
-        "dy_option": "",
-        "days": 0,
-        "end_time": 1717888888,
-        "token": "7d2d05cf08b888acaafb7f3cec59fccc813e28f9",
-        "text": "alipay",
-        "text_url": "https://shop.youneiku.com/card/routes/shop.php?action=payindex&user_id=16036"
-    }
+  code: 1,
+  data: {
+    id: 16036,
+    dy_option: "",
+    money: 0,
+    mobile: "13727968870",
+    vip_sta: "0",
+    avatar: "http://15m.xiexiebama.com/user/face/20220412/cb2476aacebd0bd86b7c4960f2b324bf.rosi",
+    token: "7d2d05cf08b888acaafb7f3cec59fccc813e28f9",
+    text: "alipay:",
+    username: "13727968870",
+    days: 0,
+    end_time: 1717776000,
+    nickname: "在我梦里",
+    text_url: "https://shop.youneiku.com/card/routes/shop.php?action=payindex&user_id=16036",
+    status: "normal"
+  },
+  msg: "",
+  time: "1717848540"
 };
+
+obj.data.money = 999;
+
 body = JSON.stringify(obj);
 $done({body});
